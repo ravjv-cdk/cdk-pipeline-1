4d355a9c404e7bd089525bd19c091b380ddf0afa
Ravichandran JV <ravjv2013@gmail.com>
2021-05-09 06:53:04 +0530

    Modified account id

--- bin/cdk-pipeline-1.ts
+++ bin/cdk-pipeline-1.ts
@@ -4,6 +4,6 @@ import { CdkPipeline1Stack } from '../lib/cdk-pipeline-1-stack'
 
 const app = new cdk.App();
 new CdkPipeline1Stack(app, 'CdkPipeline1Stack', {
-    env: { account: '069623884387', region: 'ap-south-1' },
+    env: { account: '069623884587', region: 'ap-south-1' },
 })
 app.synth()
